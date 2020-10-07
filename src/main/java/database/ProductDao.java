f596755a87f35d0ada64722432f8f675cd62f8d4
97krihop <97krihop@gmail.com>
2020-10-07 15:44:04 +0200

    added database conection and added suport for adding and listng out product table.

---
+++ src/main/java/database/ProductDao.java
@@ -0,0 +1,39 @@
+package database;
+
+import org.postgresql.ds.PGSimpleDataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+public class ProductDao {
+    public static void main(String[] args) throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/shop_app");
+        dataSource.setUser("kristianiashop");
+        dataSource.setPassword("PzBhrjmtoAaC");
+
+        System.out.println("Please enter product name:");
+        Scanner scanner = new Scanner(System.in);
+        String product = scanner.nextLine();
+
+        try(Connection connection = dataSource.getConnection()){
+            try(PreparedStatement statement = connection.prepareStatement("insert into products (name) values (?)")){
+                statement.setString(1,product);
+                statement.executeUpdate();
+            }
+        }
+
+        try(Connection connection = dataSource.getConnection()){
+            try(PreparedStatement statement = connection.prepareStatement("select * from products")){
+                try(ResultSet rs = statement.executeQuery()){
+                    while(rs.next()){
+                        System.out.println(rs.getString("name"));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/database/ProductDaoTest.java
@@ -0,0 +1,9 @@
+package database;
+
+import org.junit.jupiter.api.Test;
+
+class ProductDaoTest {
+    @Test
+    void name() {
+    }
+}
