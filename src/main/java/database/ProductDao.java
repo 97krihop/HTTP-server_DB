46c91c213718b9a58201cc47034b4ec15a219e72
97krihop <97krihop@gmail.com>
2020-10-07 17:27:47 +0200

    refactor out data sorceconstuctor in test suite and added list to real database.

--- src/main/java/database/ProductDao.java
+++ src/main/java/database/ProductDao.java
@@ -1,6 +1,8 @@
 package database;
 
 import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,7 +14,11 @@ import java.util.Scanner;
 
 public class ProductDao {
 
-    private ArrayList <String> products = new ArrayList <>();
+    private DataSource dataSource;
+
+    public ProductDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public static void main(String[] args) throws SQLException {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
@@ -42,11 +48,26 @@ public class ProductDao {
         }
     }
 
-    public void insert(String product) {
-        products.add(product);
+    public void insert(String product) throws SQLException {
+        try(Connection connection = dataSource.getConnection()){
+            try(PreparedStatement statement = connection.prepareStatement("insert into products (name) values (?)")){
+                statement.setString(1, product);
+                statement.executeUpdate();
+            }
+        }
     }
 
-    public List <String> list() {
-        return products;
+    public List <String> list() throws SQLException {
+        List<String> result = new ArrayList<>();
+        try(Connection connection = dataSource.getConnection()){
+            try(PreparedStatement statement = connection.prepareStatement("select * from products")){
+                try(ResultSet rs = statement.executeQuery()){
+                    while(rs.next()){
+                        result.add(rs.getString("name"));
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
--- src/test/java/database/ProductDaoTest.java
+++ src/test/java/database/ProductDaoTest.java
@@ -1,17 +1,30 @@
 package database;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Random;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class ProductDaoTest {
 
     @Test
-    void shouldListInsertedProducts() {
-        ProductDao productDao = new ProductDao();
+    void shouldListInsertedProducts() throws SQLException {
+        ProductDao productDao = new ProductDao(getJdbcDataSource());
         String product = exampleProduct();
         productDao.insert(product);
         assertThat(productDao.list()).contains(product);
+    }
+
+    private JdbcDataSource getJdbcDataSource() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setURL("jdbc:h2:mem:testdatabase;DB_CLOSE_DELAY=-1");
+        try(Connection connection = dataSource.getConnection()){
+            connection.prepareStatement("create table products (name varchar)").executeUpdate();
+        }
+        return dataSource;
     }
 
     private String exampleProduct() {
